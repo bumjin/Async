57dc47c158166360e659c8328e3e221e0ded9287
Beomjin Park <bumjin@gmail.com>
2024-03-10 12:34:44 +0900

    Initial commit

---
+++ src/test/java/org/example/async/AsyncApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.async;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AsyncApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
